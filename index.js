6237fed1ec3c935e7d070dd0e2127359d8e6160c
CMatchelo <cicero.marcelo.ll@gmail.com>
2021-01-25 22:00:22 -0300

    Final commit
    Projeto completo
    Concluído com o curso "Formação Node.js" pela https://www.udemy.com/course/formacao-nodejs/

--- index.js
+++ index.js
@@ -56,14 +56,32 @@ app.get("/pergunta/:id",(req, res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined) {
-            res.render("pergunta",{
-                pergunta: pergunta
+            Resposta.findAll({
+                where: {perguntaID: pergunta.id},
+                order: [
+                    ['id','DESC'] ]
+            }).then(respostas => {
+                res.render("pergunta",{
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
         }else{
             res.redirect("/");
         }
     })
 });
+
+app.post("/responder",(req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaID = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaID: perguntaID
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaID);
+    })
+})
 
 app.listen(8080,() => {
     console.log("App rodando");
